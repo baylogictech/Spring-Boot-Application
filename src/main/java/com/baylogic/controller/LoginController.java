5b392dd6f05121a397c0333036cffed19788b1ce
Lalitha <lravula@baylogictech.com>
2022-11-08 14:30:30 +0530

    removed old configuration

--- src/main/java/com/baylogic/controller/LoginController.java
+++ src/main/java/com/baylogic/controller/LoginController.java
@@ -6,60 +6,15 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import com.baylogic.model.UserLoginData;
-import com.baylogic.pojo.JwtResponse;
-import com.baylogic.pojo.User;
-import com.baylogic.securityconfig.JwtTokenUtil;
-import com.baylogic.service.JwtUserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class LoginController {
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
 
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
-
-	@Autowired
-	private JwtUserDetailsService userDetailsService;
-
-	@PostMapping(value = "/authenticate")
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody User authenticationRequest) throws Exception {
-
-		authenticate(authenticationRequest.getUserName(), authenticationRequest.getPassword());
-
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUserName());
-
-		final String token = jwtTokenUtil.generateToken(userDetails);
-
-		return ResponseEntity.ok(new JwtResponse(token));
-	}
-
-	@PostMapping(value = "/register")
-	public ResponseEntity<?> saveUser(@RequestBody UserLoginData user) throws Exception {
-		return ResponseEntity.ok(userDetailsService.save(user));
-	}
-
-	private void authenticate(String username, String password) throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
-		}
-	}
-	
 	private String getSHA3_512(String password, byte[] salt) throws NoSuchAlgorithmException {
         String generatedPassword = null;
         MessageDigest crypt = MessageDigest.getInstance("SHA3-512");
